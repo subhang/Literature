a2e4b6e3a7a4f832f8c8a5af3df7b9122739b6fd
subhang <subhang.rocklee@gmail.com>
2018-12-07 22:17:45 +0530

    distribute the cards

--- server/app.js
+++ server/app.js
@@ -4,8 +4,44 @@ var io = require('socket.io')(http);
 var player = require('./Player')
 
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function createNewDeck(players){
+
+    deck = []
+    let suits = [1,2,3,4]
+    let faces = []
+    let k = 0
+    for(let i = 1;i <= 13;i++) faces.push(i)
+    for(let i = 0;i < suits.length;i++){
+
+        for(let j = 0;j < faces.length;j++){
+
+            card = {};
+            card.suit = suits[i];
+            card.face = faces[j];
+            deck.push(card)
+        }
+    }
+    return shuffle(deck)
+}
 let connected_players = [];
 let Players = [];
 let Games  = {};
@@ -64,11 +100,28 @@ io.on('connection', function(socket){
     });
     socket.on('load_game',function(data) {
 
-        
+        let newDeck = createNewDeck();
+        let temp = Games[data.code];
+        players = shuffle(temp);
+        console.log(players)
+        for(let i = 0;i < players.length;i++){
+            players[i].team_id = i % 2
+            players[i].turn = false;
+            players[i].cards = []
+        }
+        players[0].turn = true;
+        for(let i = 0;i < newDeck.length;i++){
+            let player_id = i % (players.length);
+            newDeck[i].player_id  = player_id;
+            players[player_id ].cards.push(newDeck[i]);
+        }
+        Games[data.code] = players;
 
+        for(let i = 0;i < players.length;i++){
+            io.emit(data.code+'_'+players[i].name+'_'+'init',Games[data.code][i])
 
-
-
+        }
+        console.log(Games[data.code])
 
     });
 });
